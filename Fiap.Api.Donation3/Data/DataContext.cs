e3db78a8e3a4d9b8129e844bc2b976d65faefa7b
EduLucio-Dev <duio.el@gmail.com>
2024-10-10 16:05:21 -0300

    Iniciado o projeto e feito as migrações

---
+++ Fiap.Api.Donation3/Data/DataContext.cs
@@ -0,0 +1,37 @@
+﻿using Fiap.Api.Donation3.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fiap.Api.Donation3.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<CategoriaModel> Categorias { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<CategoriaModel>(entity =>
+            {
+                entity.ToTable("Categoria");
+                entity.HasKey(e => e.CategoriaId);
+                entity.Property(e => e.CategoriaId).ValueGeneratedOnAdd();
+                entity.Property(e => e.NomeCategoria)
+                        .IsRequired()
+                        .HasMaxLength(50);
+            });
+            modelBuilder.Entity<CategoriaModel>().HasData(
+             new CategoriaModel() { CategoriaId = 1, NomeCategoria = "Celular"},
+             new CategoriaModel() { CategoriaId = 2, NomeCategoria = "Celular" }
+            );
+        }
+
+
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected DataContext()
+        {
+        }
+    }
+}
